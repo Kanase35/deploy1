516dd53d3528c060dbc96fe261f100d03e149f74
Omkar Kanase <99755546+Kanase35@users.noreply.github.com>
2024-04-08 14:25:03 +0530

    Update settings.py
    change in setting

--- main_app/settings.py
+++ main_app/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static') 
+
+STATICFILES_DIRS = [
+    BASE_DIR,"static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
